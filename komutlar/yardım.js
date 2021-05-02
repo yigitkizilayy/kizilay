a83f15b305f07987fe95c56a532f1942157bf3ca
Glitch (kizilabeta-) <none>
2021-05-02 19:04:59 +0000

    🕵👻 Checkpoint ./komutlar/yardım.js:31469541/819

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -12,24 +12,24 @@ const mhelp = new Discord.MessageEmbed()
     .setDescription(`
 ╔═════════════════════════════
 ║   <a:dorulama:838451913988112394>     ** MODERASYON KOMUTLARI **
-║   <a:ok2:838449685697134592> k.ban    Sunucudan bir üyeyi yasaklar.
-║   <a:ok2:838449685697134592> k.unban  İstediğiniz kişinin banını kaldırır.
-║   <a:ok2:838449685697134592> k.kick   Sunucudan bir üye kickler.
-║   <a:ok2:838449685697134592> k.jail   Kişiyi jaile yollar.
-║   <a:ok2:838449685697134592> k.yetkilerim  Yetkilerini gösterir.
+║   <a:hypeshiny:838451913724264500>  k.ban    Sunucudan bir üyeyi yasaklar.
+║   <a:hypeshiny:838451913724264500>  k.unban  İstediğiniz kişinin banını kaldırır.
+║   <a:hypeshiny:838451913724264500>  k.kick   Sunucudan bir üye kickler.
+║   <a:hypeshiny:838451913724264500>  k.jail   Kişiyi jaile yollar.
+║   <a:hypeshiny:838451913724264500>  k.yetkilerim  Yetkilerini gösterir.
 ║═════════════════════════════
 ║  <a:dorulama:838451913988112394>       ** SOHBET KOMUTLARI **
-║  <a:ok2:838449685697134592> k.afk            AFK kalırsınız 
-║  <a:ok2:838449685697134592> k.sil            Belirli bir kanaldaki mesajları siler.
-║  <a:ok2:838449685697134592> k.sohbet-kapat   Sohbeti Kapatır
-║  <a:ok2:838449685697134592> k.sohbet-aç      Sohbeti Açar ve yazı yazılabilir.
-║  <a:ok2:838449685697134592> k.nuke           Kanalı kapatır,mesajları siler.
-║  <a:ok2:838449685697134592> k.mute           Kişiye chat/voice mute atılır.
+║  <a:hypeshiny:838451913724264500> k.afk            AFK kalırsınız 
+║  <a:hypeshiny:838451913724264500> k.sil            Belirli bir kanaldaki mesajları siler.
+║  <a:hypeshiny:838451913724264500> k.sohbet-kapat   Sohbeti Kapatır
+║  <a:hypeshiny:838451913724264500> k.sohbet-aç      Sohbeti Açar ve yazı yazılabilir.
+║  <a:hypeshiny:838451913724264500> k.nuke           Kanalı kapatır,mesajları siler.
+║  <a:hypeshiny:838451913724264500> k.mute           Kişiye chat/voice mute atılır.
 ║═════════════════════════════
 ║   <a:dorulama:838451913988112394>       ** SESLİ KOMUTLARI **
-║   <a:ok2:838449685697134592> k.git          Kişi onay verirse odasına gidersiniz.
-║   <a:ok2:838449685697134592> k.çek          Kişi onay verirse odanıza gelir.
-║   <a:ok2:838449685697134592> k.mute         Kişiye chat/voice mute atılır.
+║   <a:hypeshiny:838451913724264500> k.git          Kişi onay verirse odasına gidersiniz.
+║   <a:hypeshiny:838451913724264500> k.çek          Kişi onay verirse odanıza gelir.
+║   <a:hypeshiny:838451913724264500> k.mute         Kişiye chat/voice mute atılır.
 ╚═════════════════════════════
 `) 
        .addField(`» Kızılay Her gün Gellişiyor.`, `**[Destek Sunucusu](https://discord.gg/EkjJJes5)**`)
