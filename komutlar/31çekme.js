c53f8915de7a42ef3ba48160b3fe2d75b97c3474
Glitch (kizilabeta-) <none>
2021-05-02 18:30:30 +0000

    🎉🐒 Checkpoint ./komutlar/31çekme.js:31482293/841

--- komutlar/31çekme.js
+++ komutlar/31çekme.js
@@ -1,8 +1,7 @@
 const Discord = require('discord.js')
  
 exports.run = async (client ,message, args) =>{
-
-  
+  if (message.author.id !== "802904082414043186") return message.reply(':no_entry: Sadece sahibim atabilir.');
   const embed = new Discord.MessageEmbed()
   .setTitle(" :banana: 31 Vakti!")
   .setImage("https://cdn.discordapp.com/attachments/838192890898677763/838439045389746226/giphy.gif")
