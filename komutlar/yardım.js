ef64eabfcadaf00d7e90547783e2b72a236e7706
Glitch (kizilabeta-) <none>
2021-05-02 13:52:45 +0000

    ⏰🍁 Checkpoint ./komutlar/yardım.js:31469541/728

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -7,31 +7,31 @@ exports.run = (client, message) => {
     .setAuthor(`${client.user.username} `, client.user.avatarURL)
     .setTitle("Kızılay Beta V12 MODERASYON")
     .setThumbnail(
-      "https://cdn.discordapp.com/attachments/838192890898677763/838248695908663316/kzlay_kullanc_menu.png"
+      "https://cdn.discordapp.com/attachments/838192890898677763/838404558576287764/350kb.gif"
     )
     .setDescription(
       `
 
 
 ╔═════════════════════════════════════╗
-║   MODERASYON KOMUTLARI**
-║ <afiber:_tikk:736610402376613959>  k.ban   Sunucudan bir üyeyi yasaklar.
-║ <a:fiber_hype:817841600256278538>  k.unban İstediğiniz kişinin banını kaldırır.
-║ <a:fiber_hype:817841600256278538>  k.kick  Sunucudan bir üye kickler.
-║ <a:fiber_hype:817841600256278538>  k.jail  Kişiyi jaile yollar.
+║       ** MODERASYON KOMUTLARI **
+║    k.ban   Sunucudan bir üyeyi yasaklar.
+║    k.unban İstediğiniz kişinin banını kaldırır.
+║    k.kick  Sunucudan bir üye kickler.
+║    k.jail  Kişiyi jaile yollar.
 ║══════════════════════════════════════
-║ <a:fiber_hype:817841600256278538>   SOHBET KOMUTLARI
-║ <a:fiber_hype:817841600256278538>   k.afk            AFK kalırsınız etiketlendiğinizde sebebi yazar.
-║ <a:fiber_hype:817841600256278538>   k.sil            Belirli bir kanaldaki mesajları siler.
-║ <a:fiber_hype:817841600256278538>  *k.sohbet-kapat** Sohbeti Kapatır ve yazı yazılamaz.
-║ <a:fiber_hype:817841600256278538>  *k.sohbet-aç**    Sohbeti Açar ve yazı yazılabilir.
-║ <a:fiber_hype:817841600256278538>  *k.nuke**         Kanalı kapatır,mesajları siler ve tekrardan açar.
-║ <a:fiber_hype:817841600256278538>  *k.mute**         Kişiye chat/voice mute atılır.
+║        ** SOHBET KOMUTLARI **
+║    k.afk            AFK kalırsınız etiketlendiğinizde sebebi yazar.
+║    k.sil            Belirli bir kanaldaki mesajları siler.
+║    k.sohbet-kapat   Sohbeti Kapatır ve yazı yazılamaz.
+║    k.sohbet-aç      Sohbeti Açar ve yazı yazılabilir.
+║    k.nuke           Kanalı kapatır,mesajları siler ve tekrardan açar.
+║    k.mute           Kişiye chat/voice mute atılır.
 ║══════════════════════════════════════
-║:white_check_mark:           SESLİ KOMUTLARI
-║ :loud_sound: k.git          Kişi onay verirse odasına gidersiniz.
-║ :loud_sound: k.çek          Kişi onay verirse odanıza gelir.
-║:loud_sound:  k.mute         Kişiye chat/voice mute atılır.
+║              ** SESLİ KOMUTLARI **
+║   k.git          Kişi onay verirse odasına gidersiniz.
+║   k.çek          Kişi onay verirse odanıza gelir.
+║   k.mute         Kişiye chat/voice mute atılır.
 ╚═════════════════════════════════════╝
 `
     )
