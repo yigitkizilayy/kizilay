8707ab56c5c069ec5be3b8b903d1376fbadd3f27
Glitch (kizilabeta-) <none>
2021-05-02 10:48:42 +0000

    🚔🏆 Checkpoint ./komutlar/davet.js:31469541/2247 ./ayarlar.json:31469541/2 ./bot.js:31469541/1

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -1,26 +1,27 @@
-const Discord = require('discord.js')
- 
-exports.run = async (client ,message, args) =>{
+const Discord = require("discord.js");
 
-  
+exports.run = async (client, message, args) => {
   const embed = new Discord.MessageEmbed()
-  .setTitle("Davet Linkim!")
-  .setImage("https://media.giphy.com/media/Azm6adiPlNG0RHONrF/giphy.gif")
-  .setColor(0xFF0000)
-  .setDescription(`https://discord.com/oauth2/authorize?client_id=837858580799422485&scope=bot&permissions=8`)
-  .setFooter("2021 Kızılay Eğlence ve Moderasyon Botu ®")
-  message.channel.send(embed)
+    .setTitle("Senin İçin Hazırladım Buyur Davet Linkim!")
+    .setImage(
+      "https://static6.depositphotos.com/1001599/647/i/600/depositphotos_6477792-stock-photo-fire-letters-a-z.jpg"
+    )
+    .setColor(0xff0000)
+    .setDescription(
+      `https://discord.com/api/oauth2/authorize?client_id=837858580799422485&permissions=0&scope=bot`
+    )
+    .setFooter("2021 Kızılay Beta Eğlence ve Moderasyon Botu ®");
+  message.channel.send(embed);
 };
 exports.conf = {
- enabled: true,
- guildOnly: false,
- aliases: ['ping'],
- permLevel: 0
+  enabled: true,
+  guildOnly: false,
+  aliases: ["ping"],
+  permLevel: 0
 };
- 
+
 exports.help = {
- name: 'davet',
- description: 'Botun Pingine Bakarsın',
- usage: 'davet'
+  name: "davet",
+  description: "Botun Pingine Bakarsın",
+  usage: "davet"
 };
- 
