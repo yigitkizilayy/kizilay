554f49a6590dab16c5533dc507fa66efa24292a4
Glitch (kizilabeta-) <none>
2021-05-02 02:49:01 +0000

    🍕🍩 Checkpoint ./komutlar/matematik.js:31469541/1014 ./komutlar/kaçcm.js:31469541/965 ./komutlar/kurucu.js:31469541/3956

--- komutlar/kaçcm.js
+++ komutlar/kaçcm.js
@@ -1,22 +1,25 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
 exports.run = (client, message) => {
- message.channel.send({embed: {
-          color: 0xD97634,
-		  description: "**Ulan ne sapık adamsın bir de iman edip namaz kılacaksın ülkemizin haline bak ! **"
-            }});
+  message.channel.send({
+    embed: {
+      color: 0xd97634,
+      description:
+        "**Ulan ne sapık adamsın bir de iman edip namaz kılacaksın ülkemizin haline bak ! **"
+    }
+  });
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['p'],
+  aliases: ["p"],
   permLevel: 0
 };
 
 exports.help = {
-  name: 'kaçcm',
-  description: 'kedileri gösterir.',
-  usage: 'kaçcm'
+  name: "kaçcm",
+  description: "kedileri gösterir.",
+  usage: "kaçcm"
 };
