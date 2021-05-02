205d099da54250b1860349ca4b623945c83ef5ca
Glitch (kizilabeta-) <none>
2021-05-02 03:35:51 +0000

    👧🐡 Checkpoint ./komutlar/yardım.js:31469541/3650 ./komutlar/sohbet.aç.js:31469541/574 ./komutlar/sohbet-kapat.js:31469541/583 ./komutlar/mute.js:31469541/3123

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,57 +1,53 @@
-const Discord = require("discord.js");
+const Discord = require ("discord.js");
+
 exports.run = (client, message) => {
-  const motionEmbed = new Discord.MessageEmbed()
+const lembed = new Discord.MessageEmbed()
 
-    .setAuthor(`${client.user.username} `, client.user.avatarURL)
-    .setColor("RED")
-    .setThumbnail(
-      "https://im7.ezgif.com/tmp/ezgif-7-2484e616a2d9.gif"
-    )
-    .setDescription(
-      `Toplamda Botta **${client.commands.size}** Adet Komut Bulunuyor!` +
-        "\n\n `k.botbilgi`= **Yazarak Botun İstatistiklerine Ulaşa Bilirsiniz.** \n `k.davet`= **Yazarak Botun Davet Linkini Alırsınız. \n `k.oyver`= Botumuza Oy verebilirsin**"
-    )
-    .addField(
-      `:boom:➤ Komutlar`,
-      `
- 
-:gear: | **k.eğlence:** Eğlence Menüsüne Bakarsınız.
-:gear: | **k.moderasyon:** Botun Moderasyon Komutlarına Bakarsınız
-:gear: | **k.logomenü:** Botun Logo YAPMA Komutlarına Bakarsınız.
+.then;   
+const mhelp = new Discord.MessageEmbed()
+.setColor("BLACK")
+.setAuthor(`${client.user.username} `, client.user.avatarURL)  
+.setTitle("Kızılay V12 MODERASYON")
+.setThumbnail("https://cdn.discordapp.com/attachments/838192890898677763/838248695908663316/kzlay_kullanc_menu.png")
+    .setDescription(`
 
 
-`)
+╔═════════════════════════════════════╗
+║**MODERASYON KOMUTLARI**
+║**!ban**   Sunucudan bir üyeyi yasaklar.
+║**!unban** İstediğiniz kişinin banını kaldırır.
+║**!kick**  Sunucudan bir üye kickler.
+║**!jail**  Kişiyi jaile yollar.
+║══════════════════════════════════════
+║**SOHBET KOMUTLARI**
+║**!afk**          AFK kalırsınız etiketlendiğinizde sebebi yazar.
+║**!sil**          Belirli bir kanaldaki mesajları siler.
+║**!sohbet-kapat** Sohbeti Kapatır ve yazı yazılamaz.
+║**!sohbet-aç**    Sohbeti Açar ve yazı yazılabilir.
+║**!nuke**         Kanalı kapatır,mesajları siler ve tekrardan açar.
+║**!mute**         Kişiye chat/voice mute atılır.
+║══════════════════════════════════════
+║**SESLİ KOMUTLARI**
+║**!git**          Kişi onay verirse odasına gidersiniz.
+║**!çek**          Kişi onay verirse odanıza gelir.
+║**!mute**         Kişiye chat/voice mute atılır.
+╚═════════════════════════════════════╝
+`) 
+       .addField(`» Kızılay V12 MODERASYON`, `**[Destek Sunucusu](https://discord.gg/8S5Em5dUXY)**`)
+        .setFooter(`${message.author.username} Tarafından İstendi.`, message.author.avatarURL)
+message.channel.send(mhelp)
+.then; const sembed = new Discord.MessageEmbed()
 
-
-
-    .addField(
-      ` :white_check_mark: Bilgilendirme`,
-      `
-:repeat: | **Kızılay Bot Her Gün Güncelleniyor!**.
-:customs: | **Botu Ekleyerek Bize Destek Çıkmış Olursunuz.** `
-    )
-  
-  .addField(
-      `** :paperclips:➤ Bağlantılar  **`,
-      `>  » [Destek Sunucusu](https://discord.gg/TbByFaEp) \n >  » [Botun Davet Linki](https://discord.com/api/oauth2/authorize?client_id=837858580799422485&permissions=0&scope=bot) \n > » [Oy Verebilirsiniz](https://top.gg/bot/791609110658547782/vote) `
-    )
-    .setFooter(
-      `
-${message.author.username} Tarafından İstendi.`,
-      message.author.avatarURL
-    );
-  return message.channel.send(motionEmbed);
-};
-
+}; 
 exports.conf = {
-  enabled: true,
-  guildOnly: true,
-  aliases: ["h"],
-  permLevel: 0
-};
-
-exports.help = {
-  name: "yardım",
-  description: ".",
-  usage: "yardım"
-};
+    enabled: true, 
+    guildOnly: false, 
+    aliases: ["yardım"], 
+    permLevel: 0 
+  };
+ 
+  exports.help = {
+    name: 'yardım', 
+    description: '',
+    usage: ''
+  };
