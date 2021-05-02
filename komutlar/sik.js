25bb0b954d31e5b18ccaaac8437a520608c6136f
Glitch (kizilabeta-) <none>
2021-05-02 11:24:29 +0000

    👢🚀 Checkpoint ./komutlar/türk.js:31469541/1278 ./komutlar/dürt.js:31469541/1835 ./komutlar/sik.js:31469541/2290

---
+++ komutlar/sik.js
@@ -0,0 +1,79 @@
+const Discord = require('discord.js');
+exports.run = (client, message, args) => {
+
+   //CodEming     
+  
+if (message.channel.name !== '💬bot-komutları') {     
+if (!message.member.hasPermission("ADMINISTRATOR"))  {
+       message.delete()
+  var embed = new Discord.RichEmbed()
+.setColor("RED")
+.setDescription('Yanlış Yer!')
+.setThumbnail('')
+.setTimestamp()
+.setFooter(message.author.username , message.author.avatarURL)
+return message.channel.send(embed).then(msg => msg.delete(8000));
+ }
+} 
+  
+  
+
+let kullanıcı2 = message.mentions.members.first()
+let kullanıcı = message.guild.member(kullanıcı2)
+
+  let codeming = '.'
+if(!codeming) return
+  
+  if (message.mentions.users.size < 1) return message.reply('sikilcek kişiyi yazmalısın!').catch(console.error);
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  kullanıcı.send(`<@${message.author.id}>`+ " Samet Kızılay sikişe geldi 😎")
+  message.reply('Tamam Sahibim hemen yapıyorum')
+ 
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['codeming'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'sik',
+  description: 'birini dürtersiniz.',
+  usage: 'sik'
+};
