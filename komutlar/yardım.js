abc40cab1f063172ae0f801ece0827ab78566ee4
Glitch (kizilabeta-) <none>
2021-05-06 19:10:43 +0000

    ✨🚛 Checkpoint ./komutlar/yardım.js:31469541/585 ./komutlar/çağır.js:31469541/2016 ./ayarlar.json:31469541/36

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -10,28 +10,28 @@ const mhelp = new Discord.MessageEmbed()
 .setTitle("<a:online:838451914412392548> Kızılay V12 MODERASYON")
 .setThumbnail("https://cdn.discordapp.com/attachments/838192890898677763/838461528109613096/kzlay..png")
     .setDescription(`
-╔════════════════════════════
-║  <a:fiber_tikk:736610402376613959>     ** MODERASYON KOMUTLARI **
-║  <a:hypeshiny:838451913724264500>  k!moderasyon  Detaylı moderasyon komutlarını gösterir.
-║  <a:hypeshiny:838451913724264500>  k!ban   Sunucudan bir üyeyi yasaklar.
-║  <a:hypeshiny:838451913724264500>  k!unban İstediğiniz kişinin banını kaldırır.
-║  <a:hypeshiny:838451913724264500>  k!kick  Sunucudan bir üye kickler.
-║  <a:hypeshiny:838451913724264500>  k!jail  Kişiyi jaile yollar.
-║  <a:hypeshiny:838451913724264500>  k!yetkilerim  Yetkilerini gösterir.
-║════════════════════════════
-║  <a:fiber_tikk7:36610402376613959>       ** SOHBET KOMUTLARI **
-║  <a:hypeshiny:838451913724264500> k!afk           AFK kalırsınız 
-║  <a:hypeshiny:838451913724264500> k!sil           Belirli bir kanaldaki mesajları siler.
-║  <a:hypeshiny:838451913724264500> k!sohbet-kapat  Sohbeti Kapatır
-║  <a:hypeshiny:838451913724264500> k!sohbet-aç     Sohbeti Açar ve yazı yazılabilir.
-║  <a:hypeshiny:838451913724264500> k!nuke          Kanalı kapatır,mesajları siler.
-║  <a:hypeshiny:838451913724264500> k!mute          Kişiye chat/voice mute atılır.
-║════════════════════════════
+╔═════════════════════════════
+║  <a:dorulama:838451913988112394>     ** MODERASYON KOMUTLARI **
+║  <a:hypeshiny:838451913724264500> k!moderasyon moderasyon komutlarını gösterir.
+║  <a:hypeshiny:838451913724264500> k!ban Sunucudan bir üyeyi yasaklar.
+║  <a:hypeshiny:838451913724264500> k!unban İstediğiniz kişinin banını kaldırır.
+║  <a:hypeshiny:838451913724264500> k!kick  Sunucudan bir üye kickler.
+║  <a:hypeshiny:838451913724264500> k!jail  Kişiyi jaile yollar.
+║  <a:hypeshiny:838451913724264500> k!yetkilerim  Yetkilerini gösterir.
+║═════════════════════════════
+║  <a:dorulama:838451913988112394>       ** SOHBET KOMUTLARI **
+║  <a:hypeshiny:838451913724264500> k!afk            AFK kalırsınız 
+║  <a:hypeshiny:838451913724264500> k!sil            Belirli bir kanaldaki mesajları siler.
+║  <a:hypeshiny:838451913724264500> k!sohbet-kapat   Sohbeti Kapatır
+║  <a:hypeshiny:838451913724264500> k!sohbet-aç      Sohbeti Açar ve yazı yazılabilir.
+║  <a:hypeshiny:838451913724264500> k!nuke           Kanalı kapatır,mesajları siler.
+║  <a:hypeshiny:838451913724264500> k!mute           Kişiye chat/voice mute atılır.
+║═════════════════════════════
 ║  <a:dorulama:838451913988112394>       ** SESLİ KOMUTLARI **
-║  <a:hypeshiny:838451913724264500> k!git      Kişi onay verirse odasına gidersiniz.
-║  <a:hypeshiny:838451913724264500> k!çek      Kişi onay verirse odanıza gelir.
-║  <a:hypeshiny:838451913724264500> k!mute     Kişiye chat/voice mute atılır.
-╚════════════════════════════
+║  <a:hypeshiny:838451913724264500> k!git          Kişi onay verirse odasına gidersiniz.
+║  <a:hypeshiny:838451913724264500> k!çek          Kişi onay verirse odanıza gelir.
+║  <a:hypeshiny:838451913724264500> k!mute         Kişiye chat/voice mute atılır.
+╚═════════════════════════════
 `) 
        .addField(`» Kızılay Her gün Gellişiyor.`, `**[Destek Sunucusu](https://discord.gg/EkjJJes5)**`)
 .setImage("https://cdn.discordapp.com/attachments/838192890898677763/838404558576287764/350kb.gif")
